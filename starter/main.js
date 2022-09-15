25138b64e959dfc23210af56169c9931ccc70ce4
cynthia nweke <nwekecynthia2015@gmail.com>
2022-09-15 12:26:47 +0100

    completed project

---
+++ starter/main.js
@@ -0,0 +1,30 @@
+ScrollReveal({ 
+    reset: true,
+    duration: 2000,
+    distance:'70px'
+    
+ });
+ 
+ScrollReveal().reveal('.heading', {  
+ delay: 500,
+ origin:'left'
+
+ });
+ ScrollReveal().reveal('.logo, .firstsection-leftdiv p',{ 
+    delay: 500,
+    origin:"right"
+
+
+     });
+
+     ScrollReveal().reveal('.firstsection-leftdiv p', { 
+       delay: 500,
+       origin:'bottom'
+
+     });
+     
+     ScrollReveal().reveal('.footericons-container i', {
+        delay: 500,
+        origin:"top",
+        interval: 150
+        });
